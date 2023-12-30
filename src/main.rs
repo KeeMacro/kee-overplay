6a9b2476989b46dbf5e0d6bef763b323f005fc27
Michael Ritsema <michaelritsema@gmail.com>
2023-12-30 16:52:40 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+#![windows_subsystem = "windows"] // to turn off console.
+
+use egui::DragValue;
+use egui_overlay::EguiOverlay;
+#[cfg(not(target_os = "macos"))]
+use egui_render_three_d::ThreeDBackend as DefaultGfxBackend;
+#[cfg(target_os = "macos")]
+use egui_render_wgpu::WgpuBackend as DefaultGfxBackend;
+
+fn main() {
+
+    egui_overlay::start(HelloWorld { frame: 0 });
+}
+
+pub struct HelloWorld {
+    pub frame: u64,
+}
+impl EguiOverlay for HelloWorld {
+    fn gui_run(
+        &mut self,
+        egui_context: &egui::Context,
+        _default_gfx_backend: &mut DefaultGfxBackend,
+        glfw_backend: &mut egui_window_glfw_passthrough::GlfwBackend,
+    ) {
+        // just some controls to show how you can use glfw_backend
+        egui::Window::new("controls").show(egui_context, |ui| {
+            ui.set_width(300.0);
+            self.frame += 1;
+            ui.label(format!("current frame number: {}", self.frame));
+            // sometimes, you want to see the borders to understand where the overlay is.
+            let mut borders = glfw_backend.window.is_decorated();
+            if ui.checkbox(&mut borders, "window borders").changed() {
+                glfw_backend.window.set_decorated(borders);
+            }
+
+            ui.label(format!(
+                "pixels_per_virtual_unit: {}",
+                glfw_backend.physical_pixels_per_virtual_unit
+            ));
+            ui.label(format!("window scale: {}", glfw_backend.scale));
+            ui.label(format!("cursor pos x: {}", glfw_backend.cursor_pos[0]));
+            ui.label(format!("cursor pos y: {}", glfw_backend.cursor_pos[1]));
+
+            ui.label(format!(
+                "passthrough: {}",
+                glfw_backend.window.is_mouse_passthrough()
+            ));
+            // how to change size.
+            // WARNING: don't use drag value, because window size changing while dragging ui messes things up.
+            let mut size = glfw_backend.window_size_logical;
+            let mut changed = false;
+            ui.horizontal(|ui| {
+                ui.label("width: ");
+                ui.add_enabled(false, DragValue::new(&mut size[0]));
+                if ui.button("inc").clicked() {
+                    size[0] += 10.0;
+                    changed = true;
+                }
+                if ui.button("dec").clicked() {
+                    size[0] -= 10.0;
+                    changed = true;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("height: ");
+                ui.add_enabled(false, DragValue::new(&mut size[1]));
+                if ui.button("inc").clicked() {
+                    size[1] += 10.0;
+                    changed = true;
+                }
+                if ui.button("dec").clicked() {
+                    size[1] -= 10.0;
+                    changed = true;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("Buffs: ");
+                ui.add_enabled(false, DragValue::new(&mut size[1]));
+                if ui.button("Self").clicked() {
+                    println!("self buffs");
+                    changed = true;
+                }
+                if ui.button("Shields").clicked() {
+                    
+                   
+                }
+            });
+
+            if changed {
+                glfw_backend.set_window_size(size);
+            }
+        });
+
+        // here you decide if you want to be passthrough or not.
+        if egui_context.wants_pointer_input() || egui_context.wants_keyboard_input() {
+            glfw_backend.window.set_mouse_passthrough(false);
+        } else {
+            glfw_backend.window.set_mouse_passthrough(true);
+        }
+        egui_context.request_repaint();
+    }
+}
